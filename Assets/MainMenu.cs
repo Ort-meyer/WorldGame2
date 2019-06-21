64a1ca6fe65ba0b483cf24c0a0a0efa0d1b55255
john bergsten <bergsten.john92@gmail.com>
2019-06-21 11:35:41 +0200

    added some scenes and main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button m_startGameButton;
+    public Button m_quitButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_startGameButton.onClick.AddListener(delegate { M_StartGameButton(); });
+        m_quitButton.onClick.AddListener(delegate { M_QuitButton(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void M_StartGameButton()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    void M_QuitButton()
+    {
+        Application.Quit();
+    }
+}

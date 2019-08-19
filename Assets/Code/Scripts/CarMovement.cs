fcf8029bd74d211a9dbf4945c7d6ddeab0ccd104
john bergsten <bergsten.john92@gmail.com>
2019-08-19 15:44:33 +0200

    added pathfinding movement. Lots of unused code

--- Assets/Code/Scripts/CarMovement.cs
+++ Assets/Code/Scripts/CarMovement.cs
@@ -1,113 +1,167 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CarMovement : BaseMovement
 {
-    public float m_maxMoveSpeed;
-    public float m_acceleration;
-    public float m_turnTime;
 
-    public float m_breakDistance;
-    public float m_breakPower;
+    public float m_speed = 2;
+    public float m_turnSpeed = 3;
 
-    public GameObject[] frontWheels;
-    public GameObject[] rearWheels;
+    private NavMeshAgent m_agent;
+    private Vector3 m_desVelocity;
+    private CharacterController m_charControl;
 
-    // The maximum angle of the wheels
-    public float m_maxWheelAngle = 45;
+    private Vector3 m_destinationPostion;
 
-    private NavPathManager m_pathManager;
-
-    //private GameObject m_parent;
-
-    private float m_currentMoveSpeed = 0;
-    private float m_currentAngle = 0;
-    // Use this for initialization
     void Start()
     {
-        m_pathManager = GetComponent<NavPathManager>();
-        //m_parent = GetComponentInParent<BaseUnit>().gameObject;
+        m_agent = gameObject.GetComponent<NavMeshAgent>();
+        m_charControl = gameObject.GetComponent<CharacterController>();
+        m_agent.isStopped = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
 
-        if (!m_pathManager.M_DestinationReached())
-        {
-            // Get next corner
-            Vector3 nextCorner = m_pathManager.M_GetNextCorner();
-            Vector3 nextToCurrent = nextCorner - transform.position;
+        Vector3 lookPos;
+        Quaternion targetRot;
 
-            // Start breaking if we're approaching the destination
-            if (m_pathManager.M_GetDistanceToDestination() <= m_breakDistance)
-            {
-                Break();
-            }
-            else
-            {
-                TurnWheels(nextToCurrent);
-                Accelerate();
-                // Rotate car depending on wheel angle
-                Vector3 pivotPoint = transform.position; // Should be rear wheels, I reckon
-                transform.RotateAround(pivotPoint, transform.up, m_currentAngle * m_turnTime * Time.deltaTime);
-            }
-            // Move car
-            Vector3 movement = transform.forward * m_currentMoveSpeed * Time.deltaTime;
-            GetComponent<CharacterController>().Move(movement);
-        }
-        else // Hard stop
-        {
-            m_currentMoveSpeed = 0;
-        }
+        m_agent.destination = m_destinationPostion;
+        m_desVelocity = m_agent.desiredVelocity;
+
+        m_agent.updatePosition = false;
+        m_agent.updateRotation = false;
+
+        lookPos = m_destinationPostion - transform.position;
+        lookPos.y = 0;
+        targetRot = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * m_turnSpeed);
+
+        m_charControl.SimpleMove(m_desVelocity.normalized * m_speed);
+
+        m_agent.velocity = m_charControl.velocity;
     }
 
-
-    private void TurnWheels(Vector3 direction)
-    {
-        m_currentAngle = Helpers.GetDiffAngle2D(transform.forward, direction);
-        // Limit wheel angle (wheels are instant atm)
-        if (Mathf.Abs(m_currentAngle) > m_maxWheelAngle)
-        {
-            m_currentAngle = Mathf.Sign(m_currentAngle) * m_maxWheelAngle;
-        }
-
-        // Face the wheels correctly
-        Vector3 wheelForward = new Vector3(0, 0, 0);
-        foreach (GameObject obj in frontWheels)
-        {
-            obj.transform.localRotation = Quaternion.Euler(0, m_currentAngle, 0);
-            wheelForward = obj.transform.forward;
-        }
-    }
-
-    private void Accelerate()
-    {
-        // Todo: accelerate according to animation curve (quick in start, slow at end)
-        m_currentMoveSpeed += m_acceleration * Time.deltaTime;
-        if (m_currentMoveSpeed > m_maxMoveSpeed)
-        {
-            m_currentMoveSpeed = m_maxMoveSpeed;
-        }
-    }
-
-    private void Break()
-    {
-        m_currentMoveSpeed -= m_breakPower * Time.deltaTime;
-        if (m_currentMoveSpeed < 0)
-        {
-            m_currentMoveSpeed = 0;
-        }
-    }
-
+    // Sets the destination for this unit to move to
     public override void M_MoveTo(Vector3 destination)
     {
-        m_pathManager.M_SetDestination(destination);
+        m_destinationPostion = destination;
+        m_agent.isStopped = false;
     }
 
+    // Clears destination and causes the unit to stop
     public override void M_StopOrder()
     {
-        m_pathManager.M_ClearDestination();
+        m_agent.isStopped = true;
     }
+
+
+    //public float m_maxMoveSpeed;
+    //public float m_acceleration;
+    //public float m_turnTime;
+
+    //public float m_breakDistance;
+    //public float m_breakPower;
+
+    //public GameObject[] frontWheels;
+    //public GameObject[] rearWheels;
+
+    //// The maximum angle of the wheels
+    //public float m_maxWheelAngle = 45;
+
+    //private NavPathManager m_pathManager;
+
+    ////private GameObject m_parent;
+
+    //private float m_currentMoveSpeed = 0;
+    //private float m_currentAngle = 0;
+    //// Use this for initialization
+    //void Start()
+    //{
+    //    m_pathManager = GetComponent<NavPathManager>();
+    //    //m_parent = GetComponentInParent<BaseUnit>().gameObject;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //    if (!m_pathManager.M_DestinationReached())
+    //    {
+    //        // Get next corner
+    //        Vector3 nextCorner = m_pathManager.M_GetNextCorner();
+    //        Vector3 nextToCurrent = nextCorner - transform.position;
+
+    //        // Start breaking if we're approaching the destination
+    //        if (m_pathManager.M_GetDistanceToDestination() <= m_breakDistance)
+    //        {
+    //            Break();
+    //        }
+    //        else
+    //        {
+    //            TurnWheels(nextToCurrent);
+    //            Accelerate();
+    //            // Rotate car depending on wheel angle
+    //            Vector3 pivotPoint = transform.position; // Should be rear wheels, I reckon
+    //            transform.RotateAround(pivotPoint, transform.up, m_currentAngle * m_turnTime * Time.deltaTime);
+    //        }
+    //        // Move car
+    //        Vector3 movement = transform.forward * m_currentMoveSpeed * Time.deltaTime;
+    //        GetComponent<CharacterController>().Move(movement);
+    //    }
+    //    else // Hard stop
+    //    {
+    //        m_currentMoveSpeed = 0;
+    //    }
+    //}
+
+
+    //private void TurnWheels(Vector3 direction)
+    //{
+    //    m_currentAngle = Helpers.GetDiffAngle2D(transform.forward, direction);
+    //    // Limit wheel angle (wheels are instant atm)
+    //    if (Mathf.Abs(m_currentAngle) > m_maxWheelAngle)
+    //    {
+    //        m_currentAngle = Mathf.Sign(m_currentAngle) * m_maxWheelAngle;
+    //    }
+
+    //    // Face the wheels correctly
+    //    Vector3 wheelForward = new Vector3(0, 0, 0);
+    //    foreach (GameObject obj in frontWheels)
+    //    {
+    //        obj.transform.localRotation = Quaternion.Euler(0, m_currentAngle, 0);
+    //        wheelForward = obj.transform.forward;
+    //    }
+    //}
+
+    //private void Accelerate()
+    //{
+    //    // Todo: accelerate according to animation curve (quick in start, slow at end)
+    //    m_currentMoveSpeed += m_acceleration * Time.deltaTime;
+    //    if (m_currentMoveSpeed > m_maxMoveSpeed)
+    //    {
+    //        m_currentMoveSpeed = m_maxMoveSpeed;
+    //    }
+    //}
+
+    //private void Break()
+    //{
+    //    m_currentMoveSpeed -= m_breakPower * Time.deltaTime;
+    //    if (m_currentMoveSpeed < 0)
+    //    {
+    //        m_currentMoveSpeed = 0;
+    //    }
+    //}
+
+    //public override void M_MoveTo(Vector3 destination)
+    //{
+    //    m_pathManager.M_SetDestination(destination);
+    //}
+
+    //public override void M_StopOrder()
+    //{
+    //    m_pathManager.M_ClearDestination();
+    //}
 }
--- Assets/Code/Scripts/DebugMovement.cs
+++ Assets/Code/Scripts/DebugMovement.cs
@@ -1,51 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DebugMovement : BaseMovement
 {
-    // Movement speed of the unit
-    public float m_moveSpeed;
-    // How quickly the unit rotates towards forward
-    public float m_rotationSpeed;
-    // How far above the ground the unit is (this will likely be replaced)
-    public float m_hoverheight;
-    // When the distance between stopDistance and destination is this or below, the unit stops
-    public float m_stopDistance;
-    
-    // Character controller of this unit
-    private CharacterController m_controller;
-    private NavPathManager m_pathManager;
-    // Use this for initialization
+    public float m_speed = 2;
+    public float m_turnSpeed = 3;
+    NavPathManager m_pathManager;
+
     void Start()
     {
         m_pathManager = GetComponent<NavPathManager>();
-        m_controller = GetComponent<CharacterController>();
-    }
-    
-    // Update is called once per frame
-    void Update()
-    {
-        // Movement: move in XY plane, then set height and up-vector (tilt)
-        Vector3 vecToDest = m_pathManager.M_GetNextCorner() - transform.position;
-        float currentDistanceToDest = vecToDest.magnitude;
-        // If not there yet, keep moving
-        if (!(currentDistanceToDest < m_stopDistance))
-        {
-            // Move in XZ plane
-            Vector3 xyMoveVector = new Vector3(vecToDest.x, 0, vecToDest.z);
-            m_controller.SimpleMove(m_moveSpeed * xyMoveVector.normalized);
-            transform.forward = new Vector3(m_controller.velocity.x, 0, m_controller.velocity.z);
-        }
     }
 
+    void Update()
+    {
+        Vector3 vecToNextCorner = m_pathManager.M_GetNextCorner() - transform.position;
+        vecToNextCorner.y = 0;
+        transform.position += vecToNextCorner.normalized * m_speed * Time.deltaTime;
+    }
+
+    // Sets the destination for this unit to move to
     public override void M_MoveTo(Vector3 destination)
     {
         m_pathManager.M_SetDestination(destination);
     }
 
+    // Clears destination and causes the unit to stop
     public override void M_StopOrder()
     {
-        m_pathManager.M_ClearDestination();
+
     }
 }

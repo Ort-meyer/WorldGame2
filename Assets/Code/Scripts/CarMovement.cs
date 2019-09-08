de6f7b8a2826a8f9726c3f6089c8c3e0ca94b001
John Bergsten <putte_dt@hotmail.se>
2019-09-08 12:49:33 +0200

    Finally fixed car movement to avoid stutter. Still can look stupid with low steering

--- Assets/Code/Scripts/CarMovement.cs
+++ Assets/Code/Scripts/CarMovement.cs
@@ -7,10 +7,8 @@ public class CarMovement : BaseMovement
 {
 
     public float m_speed = 2;
-    //public float m_turnSpeed = 3;
+    public float m_turnSpeed = 3;
     public float m_maxWheelAngle = 30;
-    // How much the car turns towards the wheels. From 0 to 1 (0 no turning at all, 1 instantaneous)
-    //public float m_steeringFactor = 1;
     // How many degrees the car turns towards the wheels per second
     public float m_steering = 30;
     public List<GameObject> m_frontWheels;
@@ -23,12 +21,6 @@ public class CarMovement : BaseMovement
 
     private Vector3 m_destinationPostion;
 
-    //private float m_currentWheelAngle = 0;
-    //private Vector3 m_wheelForward = new Vector3(0, 0, 1);
-
-    //private float m_currentWheelAngle = 0;
-    //private float m_currentTransformAngle = 0;
-
 
     private float m_currentWheelAngle = 0;
 
@@ -36,10 +28,6 @@ public class CarMovement : BaseMovement
     {
         m_navPathManager = gameObject.GetComponent<NavPathManager>();
         m_charControl = gameObject.GetComponent<CharacterController>();
-
-        //m_wheelForward = transform.forward;
-
-        // Debug stuff
     }
 
     void Update()
@@ -56,7 +44,6 @@ public class CarMovement : BaseMovement
         {
             M_MoveTo(transform.position + transform.right * 10);
         }
-        Quaternion targetRot;
 
         if (!m_navPathManager.M_DestinationReached())
         {
@@ -67,49 +54,8 @@ public class CarMovement : BaseMovement
 
             M_TurnWheels(toNextWaypoint);
             M_TurnVehicle();
-
-            //m_charControl.SimpleMove(transform.forward.normalized * m_speed);
             m_charControl.SimpleMove(m_frontWheels[0].transform.forward.normalized * m_speed);
-
-
-            ////// Move wheels to face towards next waypoint
-            //Vector3 toNextWaypoint = m_navPathManager.M_GetNextCorner() - transform.position;
-
-            //toNextWaypoint.y = 0;
-            ////targetRot = Quaternion.LookRotation(toNextWaypoint);
-            ////transform.rotation = targetRot;
-            //m_DEBUG.transform.position = m_navPathManager.M_GetNextCorner();
-
-            //// Turn wheels
-            //M_TurnWheels(toNextWaypoint);
-
-            //// Rotate car towards wheels
-            //float diffToTarget = Helpers.GetDiffAngle2D(transform.forward, m_wheelForward);
-            ////float wheelAngle = 0; // TODO Remove this
-            //float changeAngle = Helpers.Sign(diffToTarget) * m_steering * Time.deltaTime;
-            //float newAngle = changeAngle;
-
-            //float diff = Mathf.Abs(diffToTarget) - Mathf.Abs(newAngle);
-            //if (diff < 0 && Mathf.Abs(diffToTarget) < 0.5)
-            //{
-            //    changeAngle = diffToTarget;
-            //}
-
-            ////// Limit to max wheel angle
-            //if (Mathf.Abs(newAngle) > m_maxWheelAngle)  // TODO solve bug with waypoints right behind car
-            //{
-            //    changeAngle = 0;
-            //}
-
-            //transform.Rotate(0, changeAngle, 0);
-            ////transform.Rotate(0, diffToTarget, 0);
-            ////if(Mathf.Abs(steering) > Mathf.Abs(diffAngle))
-            ////{
-            ////    transform.rotation = Quaternion.LookRotation(toNextWaypoint);
-            ////}
-            ////else
         }
-
     }
 
     // Sets the destination for this unit to move to
@@ -127,42 +73,21 @@ public class CarMovement : BaseMovement
     private void M_TurnWheels(Vector3 direction)
     {
         float diffToTarget = Helpers.GetDiffAngle2D(m_frontWheels[0].transform.forward, direction);
-        m_currentWheelAngle += diffToTarget;
+        // Set the diff angle if we risk overshooting out target (abvoids near 0 diff stutter)
+        if (Mathf.Abs(diffToTarget) < 1) // TODO improve this? Magic number is kinda bad
+        {
+            m_currentWheelAngle += diffToTarget;
+        }
+        else
+        {
+            m_currentWheelAngle += Mathf.Sign(diffToTarget) * m_turnSpeed * Time.deltaTime;
+        }
         m_currentWheelAngle = Helpers.LimitWithSign(m_currentWheelAngle, m_maxWheelAngle);
 
         foreach (GameObject obj in m_frontWheels)
         {
             obj.transform.localRotation = Quaternion.Euler(0, m_currentWheelAngle, 0);
         }
-
-
-        //float diffToTarget = Helpers.GetDiffAngle2D(m_wheelForward, direction);
-
-        //// debug
-        ////float changeAngle = diffToTarget;
-
-        //float wheelAngle = Helpers.GetDiffAngle2D(transform.forward, m_wheelForward);
-        //float changeAngle = Helpers.Sign(diffToTarget) * m_turnSpeed * Time.deltaTime;
-        //float newAngle = wheelAngle + changeAngle;
-
-        //float diff = Mathf.Abs(diffToTarget) - Mathf.Abs(newAngle);
-        //if (diff < 0 && Mathf.Abs(diffToTarget) < 0.5)
-        //{
-        //    changeAngle = diffToTarget;
-        //}
-
-        ////// Limit to max wheel angle
-        //if (Mathf.Abs(newAngle) > m_maxWheelAngle)  // TODO solve bug with waypoints right behind car
-        //{
-        //    changeAngle = 0;
-        //}
-
-        ////// Face the wheels correctly
-        //foreach (GameObject obj in m_frontWheels)
-        //{
-        //    obj.transform.Rotate(0, changeAngle, 0);
-        //}
-        //m_wheelForward = Quaternion.Euler(0, changeAngle, 0) * m_wheelForward;
     }
 
     private void M_TurnVehicle()

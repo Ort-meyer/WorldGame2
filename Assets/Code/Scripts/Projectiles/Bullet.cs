7f8127b84afc43d19416d8da18498c7382962218
John Bergsten <putte_dt@hotmail.se>
2019-11-17 14:42:25 +0100

    add explosions asset package. Spawns bullet impacts

--- Assets/Code/Scripts/Projectiles/Bullet.cs
+++ Assets/Code/Scripts/Projectiles/Bullet.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Bullet : BaseProjectile
 {
+    public GameObject m_impactEffectPrefab;
+
+    private ParticleManager m_particleManager;
 
     // Use this for initialization
     protected override void Start()
     {
-
+        m_particleManager = Object.FindObjectOfType<ParticleManager>();
     }
 
     protected override void Update()
@@ -27,11 +30,34 @@ public class Bullet : BaseProjectile
                 return;
             }
         }
+        M_SpawnImpactEffect();
         Destroy(this.gameObject);
         // Possibly helpfull code from some example somewhere
         //foreach (ContactPoint contact in collision.contacts)
         //{
         //    Debug.DrawRay(contact.point, contact.normal, Color.white);
         //}
+
+    }
+
+    void M_SpawnImpactEffect()
+    {
+        GameObject newEffect = Instantiate(m_impactEffectPrefab);
+        newEffect.transform.parent = m_particleManager.transform;
+        newEffect.transform.position = transform.position;
+        newEffect.SetActive(true);
+        ParticleSystem ps = newEffect.GetComponent<ParticleSystem>();
+
+        if (ps != null)
+        {
+            var main = ps.main;
+            if (main.loop)
+            {
+                ps.gameObject.AddComponent<CFX_AutoStopLoopedEffect>();
+                ps.gameObject.AddComponent<CFX_AutoDestructShuriken>();
+            }
+        }
+
+        m_particleManager.M_AddParticle(newEffect);
     }
 }
---
+++ Assets/ParticleManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+
+    private List<GameObject> m_activeParticleEffects = new List<GameObject>();
+    private List<GameObject> m_activeBulletHoles = new List<GameObject>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void M_AddParticle(GameObject newParticleEffect)
+    {
+        m_activeParticleEffects.Add(newParticleEffect);
+    }
+}

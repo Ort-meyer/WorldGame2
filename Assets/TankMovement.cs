03ba9cf3087c97af8e412c918a2ed0833db0588d
John Bergsten <putte_dt@hotmail.se>
2019-12-09 19:39:02 +0100

    added simple tank movement

--- Assets/TankMovement.cs
+++ Assets/TankMovement.cs
@@ -6,6 +6,8 @@ public class TankMovement : BaseMovement
 {
     public float m_speed = 2;
     public float m_turnSpeed = 3;
+    // The angle before the tank starts to move forward
+    public float m_angleToMove = 2;
 
     private NavPathManager m_navPathManager;
     private CharacterController m_charControl;
@@ -26,8 +28,27 @@ public class TankMovement : BaseMovement
             toNextWaypoint.y = 0;
 
             // Rotate to face towards target (within a certain angle maybe?)
-            
+            float turnAngle = 0;
+            float diffAngle = Helpers.GetDiffAngle2D(transform.forward, toNextWaypoint);
+
+            turnAngle = diffAngle;
+            if (Mathf.Abs(diffAngle) > m_turnSpeed * Time.deltaTime)
+            {
+                turnAngle = Helpers.Sign(diffAngle) * m_turnSpeed * Time.deltaTime;
+            }
+            transform.Rotate(new Vector3(0, turnAngle, 0));
+
             // Move forward
+            if(Mathf.Abs(diffAngle) < m_angleToMove)
+            {
+                m_charControl.SimpleMove(transform.forward * m_speed);
+            }
         }
+    }
+
+    // Sets the destination for this unit to move to
+    public override void M_MoveTo(Vector3 destination)
+    {
+        m_navPathManager.M_SetDestination(destination);
     }
 }

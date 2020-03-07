282f0639568f5947de16e9340328b1e98ac373cb
John Bergsten <putte_dt@hotmail.se>
2020-03-07 12:02:39 +0100

    work on sentences. Can load portraits

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -12,10 +12,8 @@ public class DialogueManager : MonoBehaviour
     public float m_spaceBetweenDialogues = 100;
     public Vector2 m_sentencesOffset = new Vector2(0, 0);
 
-    public GameObject m_dialoguePrefab;
-
-
-
+    public GameObject m_sentencePrefab;
+    
 
     // Use this for initialization
     void Start()
@@ -24,15 +22,24 @@ public class DialogueManager : MonoBehaviour
         string[] texts = { "Hello, how are you?", "Fine, just fine. You?", "Die motherfucker" };
         for (int i = 0; i < texts.Length; i++)
         {
-            GameObject newText = Instantiate(m_dialoguePrefab, m_dialoguePanel.GetComponent<RectTransform>());
-            newText.GetComponent<RectTransform>().localPosition = m_DialogueOffset + new Vector2(0, -m_spaceBetweenDialogues * i);
-            newText.GetComponent<Text>().text = texts[i];
+            //GameObject newText = Instantiate(m_dialoguePrefab, m_dialoguePanel.GetComponent<RectTransform>());
+            //newText.GetComponent<RectTransform>().localPosition = m_DialogueOffset + new Vector2(0, -m_spaceBetweenDialogues * i);
+            //newText.GetComponent<Text>().text = texts[i];
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
 
+            ResourceManager resourceManager = FindObjectOfType<ResourceManager>();
+            GameObject newSentenceObj = Instantiate(m_sentencePrefab, m_dialoguePanel.GetComponent<RectTransform>());
+            newSentenceObj.GetComponent<RectTransform>().localPosition = new Vector3(300, 300);
+            newSentenceObj.GetComponentInChildren<Image>().sprite = resourceManager.M_GetPortrait(0);
+            newSentenceObj.GetComponent<Text>().text = "Hello World!";
+
+        }
     }
 }
---
+++ Assets/ResourceManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceManager : MonoBehaviour
+{
+    Dictionary<int, Sprite> m_portraitImages = new Dictionary<int, Sprite>();
+    const string m_portraitsFolder = @"Graphics\Portraits\";
+    const string m_portraitMapFileName = @"portraitMap.txt";
+
+    // Use this for initialization
+    void Start()
+    {
+        M_LoadPortraits();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void M_LoadPortraits()
+    {
+        foreach (string line in File.ReadAllLines(@"Assets\Resources\" + m_portraitsFolder + m_portraitMapFileName))
+        {
+            int index = Int32.Parse(line.Split(';')[0]);
+            string portraitFileName = line.Split(';')[1];
+            //WWW www = new WWW(m_portraitsFolder + portraitFileName);
+            //while (!www.isDone)
+            //    yield return null;
+
+            Sprite portrait = Resources.Load<Sprite>(m_portraitsFolder + portraitFileName);
+            m_portraitImages.Add(index, portrait);
+        }
+    }
+
+    public Sprite M_GetPortrait(int index)
+    {
+        return m_portraitImages[index];
+    }
+}

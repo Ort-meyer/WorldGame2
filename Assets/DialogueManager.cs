06e3556c14321fa16adc6014ae4b44b27a60f7bd
John Bergsten <putte_dt@hotmail.se>
2020-03-08 12:44:23 +0100

    wip commit

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 
-
-public class Conversation
+public class PlayerSentence
 {
-
+    public bool m_active;
+    string m_text;
+    List<Sentence> m_conversation;
+    public Sentence m_sentence;
+    public PlayerSentence(string text, bool active, List<Sentence> conversation)
+    {
+        m_active = active;
+        m_text = text;
+        m_conversation = conversation;
+        m_sentence = new Sentence(text, 0);
+    }
 }
 
 // TODO move to its own file (or same as other rpg stuff?)
@@ -22,16 +32,22 @@ public class Sentence
     const int m_charsPerRow = 50; // TODO this probably isn't the best place to keep this
     public Sentence(string text, int portraitIndex)
     {
+        m_text = "";
         m_portraitIndex = portraitIndex;
-        m_text = text;
-        m_numRows = text.Length / m_charsPerRow + 1;
+        UpdateText(text);
+    }
+
+    public void UpdateText(string newText)
+    {
+        m_text = newText;
+        m_numRows = newText.Length / m_charsPerRow + 1;
     }
 }
 
 // TODO move to its own file
 public class Character
 {
-    List<Sentence> m_followUpSentences = new List<Sentence>();
+    public List<PlayerSentence> m_playerSentences = new List<PlayerSentence>();
     public Character()
     {
 
@@ -66,24 +82,56 @@ public class DialogueManager : MonoBehaviour
     // How many tows can exist at any one time. If this is exceeded, the first displayed sentence is removed
     public int m_totalNumRowsInWindow = 10;
 
-    Queue<Sentence> m_activeSentenceObjs = new Queue<Sentence>();
+    Queue<Sentence> m_activeSentences = new Queue<Sentence>();
 
     private ResourceManager m_resourceManager;
 
+    // This dude is just to test the system
+    Character m_DEBUGcharacter;
 
     private int DEBUGindex = 0;
+
+
+    private int m_numActivePlayerSentences = 0;
 
     // Use this for initialization
     void Start()
     {
+
+        for (int i = 10; i > 0; i++)
+        {
+            Debug.Log(i);
+        }
+
         m_resourceManager = FindObjectOfType<ResourceManager>();
 
-        //for (int i = 0; i < texts.Length; i++)
-        //{
-        //GameObject newText = Instantiate(m_dialoguePrefab, m_dialoguePanel.GetComponent<RectTransform>());
-        //newText.GetComponent<RectTransform>().localPosition = m_DialogueOffset + new Vector2(0, -m_spaceBetweenDialogues * i);
-        //newText.GetComponent<Text>().text = texts[i];
-        //}
+        m_DEBUGcharacter = new Character();
+        m_DEBUGcharacter.m_playerSentences = new List<PlayerSentence>()
+        {
+            new PlayerSentence("Hello", true, new List<Sentence>()
+            {
+                new Sentence("Hello", 0),
+                new Sentence("Hi there!", 1),
+            }),
+            new PlayerSentence("Are you OK?", false, new List<Sentence>()
+            {
+                new Sentence("Are you OK", 0),
+                new Sentence("No man, shit's bad. Can you help me? Some god damn dudes took some shit" +
+                "and this is just yet another long ass sentence to test the system. But that should be it", 1),
+                new Sentence("Maybe it's better to just hve multiple sentences instead of one big? I dunno", 1),
+            }),
+            new PlayerSentence("Sure, I'll help out", false, new List<Sentence>()
+            {
+                new Sentence("Sure, I'll help out", 0),
+                new Sentence("Thanks buddy! Go kick some ass", 1),
+            }),
+            new PlayerSentence("Got to go!", false, new List<Sentence>()
+            {
+                new Sentence("Got to go!", 0),
+                new Sentence("OK, cya boyo!", 1),
+            }),
+        };
+
     }
 
     // Update is called once per frame
@@ -92,76 +140,110 @@ public class DialogueManager : MonoBehaviour
 
         if (Input.GetKeyUp(KeyCode.P))
         {
-            // Obviously very debuggy
-            Sentence[] sentences =
-            {
-                // 1 is npc, 0 is player
-                new Sentence("Hey, how are you?", 1),
-                new Sentence("Fine, just fine", 0),
-                new Sentence("Good to hear! Now some really long text to test multiple lines. " +
-                             "Now some really long text to test multiple lines. " +
-                             "Now some really long text to test multiple lines.", 1),
-                new Sentence("That's good to know!", 0),
-                new Sentence("Now Im gonna say something really long!" +
-                             "Now Im gonna say something really long!" +
-                             "Now Im gonna say something really long!", 0),
-                                // 1 is npc, 0 is player
-                new Sentence("Hey, how are you?", 1),
-                new Sentence("Fine, just fine", 0),
-                new Sentence("Good to hear! Now some really long text to test multiple lines. " +
-                             "Now some really long text to test multiple lines. " +
-                             "Now some really long text to test multiple lines.", 1),
-                new Sentence("That's good to know!", 0),
-                new Sentence("Now Im gonna say something really long!" +
-                             "Now Im gonna say something really long!" +
-                             "Now Im gonna say something really long!", 0),
-            };
-            M_PutSentence(sentences[DEBUGindex]);
+            //// Obviously very debuggy
+            //Sentence[] sentences =
+            //{
+            //    // 1 is npc, 0 is player
+            //    new Sentence("Hey, how are you?", 1),
+            //    new Sentence("Fine, just fine", 0),
+            //    new Sentence("Good to hear! Now some really long text to test multiple lines. " +
+            //                 "Now some really long text to test multiple lines. " +
+            //                 "Now some really long text to test multiple lines.", 1),
+            //    new Sentence("That's good to know!", 0),
+            //    new Sentence("Now Im gonna say something really long!" +
+            //                 "Now Im gonna say something really long!" +
+            //                 "Now Im gonna say something really long!", 0),
+            //                    // 1 is npc, 0 is player
+            //    new Sentence("Hey, how are you?", 1),
+            //    new Sentence("Fine, just fine", 0),
+            //    new Sentence("Good to hear! Now some really long text to test multiple lines. " +
+            //                 "Now some really long text to test multiple lines. " +
+            //                 "Now some really long text to test multiple lines.", 1),
+            //    new Sentence("That's good to know!", 0),
+            //    new Sentence("Now Im gonna say something really long!" +
+            //                 "Now Im gonna say something really long!" +
+            //                 "Now Im gonna say something really long!", 0),
+            //};
+            //M_PutSentence(sentences[DEBUGindex]);
+            M_PutPlayerSentences();
             M_UpdateSentencePositions();
             DEBUGindex++;
         }
     }
 
-    private void M_PutSentence(Sentence sentence)
+    private GameObject M_PutSentence(Sentence sentence, GameObject sentencePrefab)
     {
-        GameObject newSentenceObj = Instantiate(m_sentencePrefab, m_conversationStartAnchor);
+        GameObject newSentenceObj = Instantiate(sentencePrefab, m_conversationStartAnchor);
         newSentenceObj.GetComponentInChildren<Image>().sprite = m_resourceManager.M_GetPortrait(sentence.m_portraitIndex);
         newSentenceObj.GetComponent<Text>().text = sentence.m_text;
 
         // Put player portrait on left side
-        if (sentence.m_portraitIndex == 0)
+        if (sentence.m_portraitIndex == 0 || sentence.m_portraitIndex == 3)
         {
             Vector3 currentPos = newSentenceObj.GetComponentInChildren<Image>().rectTransform.localPosition;
             newSentenceObj.GetComponentInChildren<Image>().rectTransform.localPosition = new Vector3(-1 * currentPos.x, currentPos.y, currentPos.z);
         }
         // If there's only one row, put it right aligned (only npc portraits)
-        if (sentence.m_numRows == 1 && sentence.m_portraitIndex !=0)
+        if (sentence.m_numRows == 1 && (sentence.m_portraitIndex != 0 || sentence.m_portraitIndex == 3))
         {
             newSentenceObj.GetComponent<Text>().alignment = TextAnchor.UpperRight;
         }
-        
-        sentence.m_sentenceObj = newSentenceObj;
-        m_activeSentenceObjs.Enqueue(sentence);
 
+        sentence.m_sentenceObj = newSentenceObj;
+        m_activeSentences.Enqueue(sentence);
+        return newSentenceObj;
     }
 
+    private void M_PutPlayerSentences()
+    {
+        int sentenceIndex = 1;
+        foreach (PlayerSentence playerSentence in m_DEBUGcharacter.m_playerSentences)
+        {
+            if (playerSentence.m_active)
+            {
+                playerSentence.m_sentence.UpdateText(sentenceIndex.ToString() + ": " + playerSentence.m_sentence.m_text);
+                sentenceIndex ++;
+                GameObject playerSentenceObj = M_PutSentence(playerSentence.m_sentence, m_playerSentencePrefab);
+                playerSentenceObj.GetComponent<Button>().onClick.AddListener(delegate { M_PlayerSentenceOnClick(playerSentence.m_sentence.m_text); });
+            }
+        }
+        m_numActivePlayerSentences = sentenceIndex;
+    }
+
+    public void M_PlayerSentenceOnClick(string sentenceString)
+    {
+        // Remove all player sentence objects
+        for (int i = m_activeSentences.Count; i > 0; i++)
+        {
+
+        }
+
+        // Convert queue to array, remove last few sentences, then back to queue
+        m_activeSentences = new Queue<Sentence>(m_activeSentences.ToArray().Skip(m_activeSentences.Count - m_numActivePlayerSentences));
+        
+
+
+        M_UpdateSentencePositions();
+    }
+    
+    // Updates the positions of all sentences. TODO consider refactoring to always clean and add sentence objects
     private void M_UpdateSentencePositions()
     {
         // Iterate from back of queue and put most recent messages first. If full, remove from queue
         int currentRow = 0;
         //Sentence[] sentences = new Sentence[m_activeSentenceObjs.Count];
-        Sentence[] sentences = m_activeSentenceObjs.ToArray();
+        Sentence[] sentences = m_activeSentences.ToArray();
 
         float previousYPos = 0;
 
-        for (int i = m_activeSentenceObjs.Count; i > 0; i--)
+        for (int i = m_activeSentences.Count; i > 0; i--)
         {
             Sentence currentSentence = sentences[i - 1];
             currentRow += currentSentence.m_numRows;
             // Full, remove from queue
             if (currentRow > m_totalNumRowsInWindow)
             {
-                Destroy(m_activeSentenceObjs.Dequeue().m_sentenceObj);
+                Destroy(m_activeSentences.Dequeue().m_sentenceObj);
             }
             // Print this sentence
             else
